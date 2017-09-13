0590653c9684e6d871339419ebcca8d37bafd901
weijchen <weijchen0919@gmail.com>
2017-09-13 13:19:37 +0800

    modify tab

--- src/app.js
+++ src/app.js
@@ -7,15 +7,14 @@ import fetch from 'isomorphic-fetch'
 import Sandbox from 'sandbox'
 import http from 'http'
 
-// import dotenv from 'dotenv'
-//
-// dotenv.load()
+import dotenv from 'dotenv'
+dotenv.load()
 
 const s = new Sandbox()
 
 const logger = (...textArgs) => {
-  const date = D.format('YYYY-MM-DD hh:mm:ss', new Date())
-  console.log(`${chalk.gray(`[${date}]`)} - ${textArgs.join(' ')}`)
+    const date = D.format('YYYY-MM-DD hh:mm:ss', new Date())
+    console.log(`${chalk.gray(`[${date}]`)} - ${textArgs.join(' ')}`)
 }
 
 const { EMAIL, PASSWORD, APP_NAME = 'pickonebot' } = process.env
@@ -25,64 +24,64 @@ if (!APP_NAME) throw Error('Missing app name')
 let leetcodeTable = []
 
 const updateLeetcodeTable = () => {
-  logger('Start updating')
-  return fetch('https://leetcode.com/api/problems/algorithms/')
-  .then(response => response.json())
-  .then(jsonData => {
-    leetcodeTable = jsonData.stat_status_pairs
-    logger(`Leetcode table constructed: number of problems is ${R.length(leetcodeTable)}`)
-  })
-  .catch(e => logger(e))
+    logger('Start updating')
+    return fetch('https://leetcode.com/api/problems/algorithms/')
+        .then(response => response.json())
+        .then(jsonData => {
+            leetcodeTable = jsonData.stat_status_pairs
+            logger(`Leetcode table constructed: number of problems is ${R.length(leetcodeTable)}`)
+        })
+        .catch(e => logger(e))
 }
 
 const getDifficulty = (levelText) => ({
-  easy: 1,
-  medium: 2,
-  hard: 3,
+    easy: 1,
+    medium: 2,
+    hard: 3,
 })[levelText]
 
 const pickOne = (levelText = 'easy') => {
-  const level = R.ifElse(
-    R.flip(R.contains)(['easy', 'medium', 'hard']),
-    getDifficulty,
-    R.always('easy')
-  )(levelText)
+    const level = R.ifElse(
+        R.flip(R.contains)(['easy', 'medium', 'hard']),
+        getDifficulty,
+        R.always('easy')
+    )(levelText)
 
-  const availableQuestions = R.filter(
-    R.and(
-      R.propEq('paid_only', false),
-      R.pathEq(['difficulty', 'level'], level)
-    ))(leetcodeTable)
-  const length = R.length(availableQuestions)
-  const randomIndex = Math.floor(Math.random() * (length - 1))
-  // logger(availableQuestions[randomIndex])
-  return availableQuestions[randomIndex]
+    const availableQuestions = R.filter(
+        R.and(
+            R.propEq('paid_only', false),
+            R.pathEq(['difficulty', 'level'], level)
+        ))(leetcodeTable)
+    const length = R.length(availableQuestions)
+    const randomIndex = Math.floor(Math.random() * (length - 1))
+    // logger(availableQuestions[randomIndex])
+    return availableQuestions[randomIndex]
 }
 
 const parseQuestion = (question) => {
-  const baseUrl = 'https://leetcode.com/problems/'
-  const url = `${baseUrl}${R.path(['stat', 'question__title_slug'], question)}`
-  const questionTitle = R.path(['stat', 'question__title'], question)
-  const accept = R.path(['stat', 'total_acs'], question)
-  const submit = R.path(['stat', 'total_submitted'], question)
-  const acceptRate = `${Math.round((accept / submit) * 10000) / 100}% (${accept}/${submit})`
-  return {
-    url,
-    questionTitle,
-    acceptRate,
-  }
+    const baseUrl = 'https://leetcode.com/problems/'
+    const url = `${baseUrl}${R.path(['stat', 'question__title_slug'], question)}`
+    const questionTitle = R.path(['stat', 'question__title'], question)
+    const accept = R.path(['stat', 'total_acs'], question)
+    const submit = R.path(['stat', 'total_submitted'], question)
+    const acceptRate = `${Math.round((accept / submit) * 10000) / 100}% (${accept}/${submit})`
+    return {
+        url,
+        questionTitle,
+        acceptRate,
+    }
 }
 
 updateLeetcodeTable()
 //
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('')
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('')
 }).listen(process.env.PORT || 5000)
 
 setInterval(() => {
-  http.get(`http://${APP_NAME}.herokuapp.com/`, () => {})
-  updateLeetcodeTable()
+    http.get(`http://${APP_NAME}.herokuapp.com/`, () => {})
+    updateLeetcodeTable()
 }, (600000 * Math.random()) + 600000) // between 20 and 50 min prevent from hault
 
 // const whiteList = ['619160284'] // Lulala Chen
@@ -94,96 +93,98 @@ const threadsCurrentSession = new Set()
 
 let threadIdLastSession
 try {
-  threadIdLastSession = new Set(JSON.parse(fs.readFileSync('threadIdLastSession.json', 'utf8')))
+    threadIdLastSession = new Set(JSON.parse(fs.readFileSync('threadIdLastSession.json', 'utf8')))
 } catch (e) {
-  threadIdLastSession = new Set()
-  fs.appendFileSync('threadIdLastSession.json', '[]')
+    threadIdLastSession = new Set()
+    fs.appendFileSync('threadIdLastSession.json', '[]')
 }
 
 // 1478245778936154 -> SexyCoding
 process.on('SIGINT', () => {
-  fs.writeFileSync('threadIdLastSession.json', JSON.stringify(Array.from(threadsCurrentSession)))
-  sayGoodbye()
-  setTimeout(() => process.exit(), 1000)
+    fs.writeFileSync('threadIdLastSession.json', JSON.stringify(Array.from(threadsCurrentSession)))
+    sayGoodbye()
+    setTimeout(() => process.exit(), 1000)
 })
 
 let appState
 try {
-  appState = JSON.parse(fs.readFileSync('appstate.json', 'utf8'))
+    appState = JSON.parse(fs.readFileSync('appstate.json', 'utf8'))
 } catch (e) {
-  appState = {}
-  fs.appendFileSync('appstate.json', '{}')
+    appState = {}
+    fs.appendFileSync('appstate.json', '{}')
 }
 
 
 const credentials = R.ifElse(
-  R.equals({}),
-  R.always({
-    email: EMAIL,
-    password: PASSWORD,
-  }),
-  R.objOf('appState')
+    R.equals({}),
+    R.always({
+        email: EMAIL,
+        password: PASSWORD,
+    }),
+    R.objOf('appState')
 )(appState)
 logger(`Login with: ${appState === {} ? 'email/password' : 'credentials'}`)
 
 login(credentials, {
-  forceLogin: true,
+    forceLogin: true,
 }, (err, api) => {
-  fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()))
-  const sayHello = () => {
-    threadIdLastSession.forEach((threadID) =>
-      api.sendMessage('我又回來啦～喵', threadID)
-    )
-  }
-  sayHello()
-  sayGoodbye = () => {
-    threadsCurrentSession.forEach((threadID) =>
-      api.sendMessage('我要下線啦～喵', threadID)
-    )
-  }
-  if (err) return console.error(err)
-  api.setOptions({ selfListen: true, logLevel: 'silent' })
-  api.listen((error, event = {}) => {
-    const currentUserId = api.getCurrentUserID()
-    const { type, senderID, body = '', threadID, attachments = [{}] } = event
-    const { stickerID = 'No sticker' } = R.pathOr({}, [0])(attachments)
-    threadsCurrentSession.add(threadID)
-    logger(type, senderID, body, threadID, stickerID)
-    if (stickerID !== 'No sticker' && senderID !== currentUserId) {
-      api.sendMessage({ sticker: stickerID }, threadID)
+    fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()))
+    const sayHello = () => {
+        threadIdLastSession.forEach((threadID) =>
+            api.sendMessage('我又回來啦～喵', threadID)
+        )
     }
-    const [command, ...argvs] = body.split(' ')
-    switch (command.toLowerCase()) {
-      case 'pickone':
-      case '/leetcode': {
-        const endTyping = api.sendTypingIndicator(threadID, logger)
-        const difficulty = (R.pathOr('Easy', [0], argvs)).toLowerCase()
-        const {
-          url,
-          questionTitle,
-          acceptRate,
-        } = parseQuestion(pickOne(difficulty))
-        api.sendMessage({ sticker: '1398251827059667' }, threadID)
-        api.sendMessage({
-          url,
-          body: `It's ${difficulty}! \n題目：${questionTitle} \n通過率：${acceptRate} \n大家加油！`,
-        }, threadID)
-        endTyping()
-        break
-      }
-      case '/sandbox': {
-        const code = body.replace('/sandbox')
-        api.sendMessage('正在努力計算！喵', threadID)
-        const endTyping = api.sendTypingIndicator(threadID, logger)
-        s.run(code, ({ result, console }) => {
-          endTyping()
-          api.sendMessage(`結果：${result}`, threadID)
-          api.sendMessage(`Log：${console.join(',')}`, threadID)
-        })
-        break
-      }
-      default:
-        return
+    sayHello()
+    sayGoodbye = () => {
+        threadsCurrentSession.forEach((threadID) =>
+            api.sendMessage('我要下線啦～喵', threadID)
+        )
     }
-  })
+    if (err) return console.error(err)
+    api.setOptions({ selfListen: true, logLevel: 'silent' })
+    api.listen((error, event = {}) => {
+        const currentUserId = api.getCurrentUserID()
+        const { type, senderID, body = '', threadID, attachments = [{}] } = event
+        const { stickerID = 'No sticker' } = R.pathOr({}, [0])(attachments)
+        threadsCurrentSession.add(threadID)
+        logger(type, senderID, body, threadID, stickerID)
+        if (stickerID !== 'No sticker' && senderID !== currentUserId) {
+            api.sendMessage({ sticker: stickerID }, threadID)
+        }
+        const [command, ...argvs] = body.split(' ')
+        switch (command.toLowerCase()) {
+            case 'pickone':
+            case '/leetcode':
+                {
+                    const endTyping = api.sendTypingIndicator(threadID, logger)
+                    const difficulty = (R.pathOr('Easy', [0], argvs)).toLowerCase()
+                    const {
+                        url,
+                        questionTitle,
+                        acceptRate,
+                    } = parseQuestion(pickOne(difficulty))
+                    api.sendMessage({ sticker: '1398251827059667' }, threadID)
+                    api.sendMessage({
+                        url,
+                        body: `It's ${difficulty}! \n題目：${questionTitle} \n通過率：${acceptRate} \n大家加油！`,
+                    }, threadID)
+                    endTyping()
+                    break
+                }
+            case '/sandbox':
+                {
+                    const code = body.replace('/sandbox')
+                    api.sendMessage('正在努力計算！喵', threadID)
+                    const endTyping = api.sendTypingIndicator(threadID, logger)
+                    s.run(code, ({ result, console }) => {
+                        endTyping()
+                        api.sendMessage(`結果：${result}`, threadID)
+                        api.sendMessage(`Log：${console.join(',')}`, threadID)
+                    })
+                    break
+                }
+            default:
+                return
+        }
+    })
 })
--- src/convertTableToJson.js
+++ src/convertTableToJson.js
@@ -3,38 +3,38 @@ import cheerio from 'cheerio'
 const LEETCODE_ROOT = 'https://leetcode.com'
 
 const convertTableToJson = (html) => {
-  const $ = cheerio.load(html)
+    const $ = cheerio.load(html)
 
-  const tableAsJson = []
+    const tableAsJson = []
 
-  const columnHeadings = []
-  $(html).find('tr').each((i, row) => {
-    $(row).find('th').each((j, cell) => {
-      columnHeadings[j] = $(cell).text().trim()
-    })
-  })
-
-  $(html).find('tr').each((i, row) => {
-    const rowAsJson = {}
-    $(row).find('td').each((j, cell) => {
-      const columnHeader = columnHeadings[j]
-      if (columnHeader === 'Title') {
-        rowAsJson[columnHeader] = $(cell).text().trim()
-        const link = $(cell).children('a').attr('href')
-        rowAsJson.link = `${LEETCODE_ROOT}${link}`
-      } else if (columnHeader) {
-        rowAsJson[columnHeader] = $(cell).text().trim()
-      } else {
-        rowAsJson[j] = $(cell).text().trim()
-      }
+    const columnHeadings = []
+    $(html).find('tr').each((i, row) => {
+        $(row).find('th').each((j, cell) => {
+            columnHeadings[j] = $(cell).text().trim()
+        })
     })
 
-    if (rowAsJson !== {}) {
-      tableAsJson.push(rowAsJson)
-    }
-  })
+    $(html).find('tr').each((i, row) => {
+        const rowAsJson = {}
+        $(row).find('td').each((j, cell) => {
+            const columnHeader = columnHeadings[j]
+            if (columnHeader === 'Title') {
+                rowAsJson[columnHeader] = $(cell).text().trim()
+                const link = $(cell).children('a').attr('href')
+                rowAsJson.link = `${LEETCODE_ROOT}${link}`
+            } else if (columnHeader) {
+                rowAsJson[columnHeader] = $(cell).text().trim()
+            } else {
+                rowAsJson[j] = $(cell).text().trim()
+            }
+        })
 
-  return tableAsJson
+        if (rowAsJson !== {}) {
+            tableAsJson.push(rowAsJson)
+        }
+    })
+
+    return tableAsJson
 }
 
 export default convertTableToJson
